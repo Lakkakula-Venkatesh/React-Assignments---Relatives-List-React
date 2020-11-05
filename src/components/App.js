c5e356276dc0474e8eb848657bcfdbd7528a04d9
Lakkakula-Venkatesh <68702624+Lakkakula-Venkatesh@users.noreply.github.com>
2020-11-05 16:11:24 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    const list = ["a", "b", "c", "d"];
+    return (
+      <div id="main">
+        {/* Do not remove the main div */}
+        <ol key="relativeList">
+          {list.map((item, index) => (
+            <li key={`relativeList${index + 1}`}></li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
